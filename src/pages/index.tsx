88fa2bebdf87129e31ec963a69b098f8e4c1d4fb
luan cruz <luanncruzz20@gmail.com>
2024-07-03 21:44:58 -0400

    init button created and outline variant made

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <h1 className="font-custom font-bold text-xl">Landpage Restaurant</h1>
+
+        <Button variant={"outline"}>Our Menu</Button>
+      {/* <h1 className="font-custom font-bold text-xl">Landpage Restaurant</h1>
 
       <div className="flex items-center">
         <div className="bg-custom-primary-f w-10 h-10"></div>
@@ -17,7 +20,9 @@ export default function Home() {
         <div className="bg-custom-secondary-q w-10 h-10"></div>
         <div className="bg-custom-secondary-h w-10 h-10"></div>
         <div className="bg-custom-secondary-w w-10 h-10"></div>
-      </div>
+      </div> */}
+
+
     </main>
   );
 }
