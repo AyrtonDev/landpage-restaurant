c8c086b05372b42c25c5362c232abdd0e5244fb0
Ayrton Guerra <ayrtong.guerra@gmail.com>
2024-07-04 22:43:14 -0300

    create routes, webcontent constants

---
+++ src/layout/header/constants/index.tsx
@@ -0,0 +1,14 @@
+export const headerLinks = [
+  {
+    href: '/about',
+    label: 'about',
+  },
+  {
+    href: '/menu',
+    label: 'menu',
+  },
+  {
+    href: '/venue',
+    label: 'venue',
+  },
+];
